6500a22b069134211003f83f826e5935604c07d2
subu00 <73189091+subu00@users.noreply.github.com>
2021-09-18 23:08:30 +0545

    remodeled js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const initialPrice = document.querySelector('#initial-price')
 const numberOfStocks = document.querySelector("#number-stocks")
 const currentPrice = document.querySelector("#current-price")
-const button = document.querySelector(".btn")
+const button = document.querySelector(".btn-calculate")
 const result = document.querySelector('.result')
-const div = document.querySelector(".content h1")
 
 button.addEventListener('click', submitHandler);
 
@@ -20,9 +19,14 @@ function submitHandler() {
 
 function calculateProfitAndLoss(initial,quantity, current) {
 
+    if (initial === "" || current === "" || quantity === "") {
+        showOutput("Inital Rate / Quantity of Stocks / Current Rate cannot be empty")
+    } else if( Number(initial) <=0 || Number(quantity) <=0) {
+        showOutput("Initial rate OR quantity cannot be zero or less than zero")
+    }
 
 
-       if(initial > current) {
+       else if(initial > current) {
            
            let loss = (initial - current) * quantity
            let lossPercentage = Math.round((loss /initial) * 100);
@@ -45,6 +49,6 @@ function calculateProfitAndLoss(initial,quantity, current) {
 
 
 function showOutput(message) {
-    div.innerHTML = message;
-    result.innerHTML = message;
+
+    result.textContent = message;
 }
