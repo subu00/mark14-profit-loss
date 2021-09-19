ee6d155dc67900609d829f27d947a0e2298dc529
subu00 <73189091+subu00@users.noreply.github.com>
2021-09-19 09:46:14 +0545

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function calculateProfitAndLoss(initial,quantity, current) {
     if (initial === "" || current === "" || quantity === "") {
         showOutput("Inital Price / Quantity of Stocks / Current Price cannot be empty")
     } else if( Number(initial) <=0 || Number(quantity) <=0 || Number(current) <=0 ) {
-        showOutput("Please put a positive value for Inital Value or Quantity of Stocks or current price")
+        showOutput("Please put a positive value for Initial Value or Quantity of Stocks or Current price")
     }
 
 
@@ -37,7 +37,7 @@ function calculateProfitAndLoss(initial,quantity, current) {
 
            let profit = (current - initial) * quantity;
            let profitPercentage = Math.round((profit/ initial) * 100)
-           showOutput(`The total Profit is  ₹${profit} and percentage is ${profitPercentage}💸💸💸💸💸 `) 
+           showOutput(`The total Profit is  ₹${profit} and percentage is ${profitPercentage}% 💸💸💸💸💸 `) 
           
        } else {
            // the rest of the logic  
